6931ac2fdc1e0bc054277e4f9e558163459b7cef
Akira Chiku <akira.chiku@gmail.com>
2015-09-15 13:00:23 +0900

    Initial commit

---
+++ hipnotify/__init__.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+
+__author__ = 'Akira Chiku'
+__email__ = 'akira.chiku@gmail.com'
+__version__ = '0.1.0'
---
+++ tests/test_hipnotify.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+test_hipnotify
+----------------------------------
+
+Tests for `hipnotify` module.
+"""
+
+import unittest
+
+from hipnotify import hipnotify
+
+
+class TestHipnotify(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def test_something(self):
+        pass
+
+    def tearDown(self):
+        pass
